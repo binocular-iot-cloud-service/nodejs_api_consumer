83538e8cec1960038af0cf473032e2007aadef08
sebastian jonasson <sebastianjonasson@gmail.com>
2015-04-17 09:53:15 +0200

    Initial commit

---
+++ src/binocular-data-services/get-device-types.js
@@ -0,0 +1,3 @@
+exports.getDeviceTypes = function() {
+	http.get('/devicetypes?api_key=55285bd1018d767753bf0045',console.log, console.log);
+}
---
+++ src/binocular-data-services/get-devices.js
@@ -0,0 +1,8 @@
+/**
+  * Create an html output file
+  * @param outputdir directory to put the file
+  * @param htmlfile name of the file
+*/
+exports.getDevices = function() {
+	//http.get(...); //agains
+}
