a7bc8e62a750afb18155270075ed280476a685d7
sebastian jonasson <sebastianjonasson@gmail.com>
2015-04-21 14:55:52 +0200

    Refractoring

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var _interface = require('./binocular-data-service')
 
 module.exports = function() {
-	this.setApiKey = function() {
+	this.setCredentials = function(api_key, client_secret) {
+		var config = {
+			API_KEY: api_key,
+			CLIENT_SECRET: client_secret,
+			host: "https://api.binocular.se/v1"
+		};
 
+		var content = JSON.stringify(config);
+
+		var fs = require('fs');
+		fs.writeFile(__dirname+"/config.json", content, function(err) {
+		    if(err) {
+		        return console.log(err);
+		    }
+		}); 
+
+	};
+
+	for(var obj in _interface) {
+		this[obj] = obj;
 	}
-	this.setClientSecret = function() {
-		
-	}
-	this.api = _interface;
 }
